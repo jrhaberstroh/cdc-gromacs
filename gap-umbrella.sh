d84397cd196a2f62f7eee6e415d6886c5c03a569
John Haberstroh <jhaberstroh@berkeley.edu>
2015-09-22 13:43:38 -0700

    Updated gap-umbrella.sh to include help option and text

--- gap-umbrella.sh
+++ gap-umbrella.sh
@@ -6,12 +6,32 @@ set -o errexit
 SRCDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $SRCDIR
 
+# \t is delimiter, do not use tabs in HELPTEXT
+read -r -d'\t' HELPTEXT << help 
+Options:
+    GROMPP    grompp executable
+    MDRUN     umbrella mdrun executable
+    G_ENERGY  g_energy executable
+    BIAS      bias value, %4.2f
+    CHROMO    chromophore for bias, [367,373]
+    OUTDIR    output directory
+    MDPIN     input mdp file (see mdp/test.mdp for template)\t
+help
+
+if [ ${1-} = '-h' ]; then
+    echo "$HELPTEXT"
+    exit 0
+fi
+
+   
+
 GROMPP=${GROMPP-grompp_sp}
 MDRUN=${MDRUN-$SRCDIR/mod-gromacs-4.6.7/build/src/kernel/mdrun}
 G_ENERGY=${G_ENERGY-g_energy_sp}
 BIAS=${BIAS-0}
 CHROMO=${CHROMO-367}
 OUTDIR=${OUTDIR-$SRCDIR}
+MDPIN=${MDPIN-$SRCDIR/mdp/test.mdp}
 dir=$OUTDIR/testdir_$CHROMO
 BIASSTR=$(printf '%+4.2f' $BIAS | sed 's/+/P/' | sed 's/-/M/')
 BIASSTR=$(echo $BIASSTR | sed 's/\./p/')
@@ -21,11 +41,13 @@ if [ ! -e $dir ]; then
     mkdir $dir
 fi
 
-sed -i "s/pull-group1.*/pull-group1 = BCL_BCL_$CHROMO/" mdp/test.mdp
-sed -i "s/pull-k1.*/pull-k1 = $BIAS/" mdp/test.mdp
+MDP=$dir/$file.mdp
+cp $MDPIN $MDP
+sed -i "s/pull-group1.*/pull-group1 = BCL_BCL_$CHROMO/" $MDP
+sed -i "s/pull-k1.*/pull-k1 = $BIAS/" $MDP
 
 $GROMPP -c FMO_conf/em/em.gro -n FMO_conf/index.ndx -p FMO_conf/4BCL.top \
-        -f mdp/test.mdp -o $dir/$file -po $dir/$file -maxwarn 1
+        -f $MDP -o $dir/$file -po $dir/$file -maxwarn 1
 $MDRUN -nt 1 -v -deffnm $dir/$file
 
 cd $dir
