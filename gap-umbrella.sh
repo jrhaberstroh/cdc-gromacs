b09345d54725d934f9cdeeb84cf631458a742c9d
John Haberstroh <jhaberstroh@berkeley.edu>
2015-10-14 13:47:30 -0700

    updated gap-umbrella to have a RESTART flag instead of the more ambiguous RESTART_COUNTER flag

--- gap-umbrella.sh
+++ gap-umbrella.sh
@@ -18,6 +18,7 @@ Options:
     OUTDIR    output directory
     INDEX     index group file from forcefield with BCL_BCL_# entries
     TOP       forcefield topology
+    RESTART   (true/false): run job as a restart?
     MDPIN     input mdp file (see mdp/test.mdp for template)\t
 help
 
@@ -37,27 +38,30 @@ OUTDIR=${OUTDIR-$SRCDIR}
 MDPIN=${MDPIN-$SRCDIR/mdp/test.mdp}
 INDEX=${INDEX-$SRCDIR/FMO_conf/index.ndx}
 TOP=${TOP-$SRCDIR/FMO_conf/4BCL.top}
+RESTART=${RESTART-false}
 dir=$OUTDIR/testdir_$CHROMO
 BIASSTR=$(printf '%+4.2f' $BIAS | sed 's/+/P/' | sed 's/-/M/')
 BIASSTR=$(echo $BIASSTR | sed 's/\./p/')
 file=bias_$BIASSTR
 
-if [ ! -e $dir ]; then
-    mkdir $dir
-fi
-
-MDP=$dir/$file.mdp
-cp $MDPIN $MDP
-sed -i "s/pull-group1.*/pull-group1 = BCL_BCL_$CHROMO/" $MDP
-sed -i "s/pull-k1.*/pull-k1 = $BIAS/" $MDP
-
-$GROMPP -c $START -n $INDEX -p $TOP \
-        -f $MDP -o $dir/$file -po $dir/$file -maxwarn 1
-$MDRUN -v -deffnm $dir/$file
+if [ "$RESTART" = 'true' ]; then
+    $MDRUN -v -s $dir/$file.tpr -cpi $dir/$file.cpt
+else
+    if [ ! -e $dir ]; then
+        mkdir $dir
+    fi
+    MDP=$dir/$file.mdp
+    cp $MDPIN $MDP
+    sed -i "s/pull-group1.*/pull-group1 = BCL_BCL_$CHROMO/" $MDP
+    sed -i "s/pull-k1.*/pull-k1 = $BIAS/" $MDP
+    $GROMPP -c $START -n $INDEX -p $TOP \
+            -f $MDP -o $dir/$file -po $dir/$file -maxwarn 1
+    $MDRUN -v -deffnm $dir/$file -cpt 1
+fi 
 
 cd $dir
 $G_ENERGY -f $file.edr -o $file-gap.xvg <<< COM-Pull-En
-TWONUMMATCH="^ \+[0-9.]\+ \+[0-9.]\+ *$"
+TWONUMMATCH="^ \+[0-9.]\+ \+\-\?[0-9.]\+ *$"
 cat $file-gap.xvg | grep -v "$TWONUMMATCH" \
         > $file-gap_cm.xvg
 cat $file-gap.xvg | grep "$TWONUMMATCH" | awk '{print "    " $1 "    " $2*349.757}' \
