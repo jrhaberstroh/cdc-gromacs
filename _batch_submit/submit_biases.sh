fd48d0a52ec409f903369b0f1123fade1f7233c3
John Haberstroh <jhaberstroh@berkeley.edu>
2015-10-27 15:24:03 -0700

    upgrading submit_biases.sh to create a self copy when originally submitted so that future modifications to the base source do not modify ongoing behavior

--- _batch_submit/submit_biases.sh
+++ _batch_submit/submit_biases.sh
@@ -7,17 +7,24 @@ set -o errexit
 ################################################################################
 #  CONFIGURATION
 ################################################################################
-OUTDIR=$SCRATCH/2015-10-FmoUmbrella2/16-production
+# Manual config
+OUTDIR=$SCRATCH/2015-10-FmoUmbrella2/27-debug
 UMBRELLA=$HOME/Code/umbrellamacs2/gap-umbrella.sh
 STARTDIR=$SCRATCH/2015-07-FmoEd/eq4-md_long/starts
 MAX_REPEAT=100
-
 njobs=${njobs-24}
+name=bias
+
+# Automatic config
 nnodes=$(( ($njobs + 23) / 24 ))
 width=$(( $nnodes * 24 ))
-name=umbrellamacs
-
-SRCDIR=${PBS_O_WORKDIR-$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )}
+SRCDIR=${PBS_O_WORKDIR+$OUTDIR}
+if [ -z $SRCDIR ]; then
+    SRCDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+    SRCNAME=$(basename ${BASH_SOURCE[0]} | sed 's/\.sh//')
+fi
+SRCNAME=${SRCNAME?No SRCNAME set, this should be automatic!}
+# Must stay set for configuration to be relative to submit folder???
 cd $SRCDIR
 ################################################################################
 #  Perform initial submission
@@ -37,14 +44,18 @@ if [ -z ${PBS_JOBID+x} ]; then
 	WALLTIME="02:00:00"
 	QUEUE="regular"
     fi
-    qsub -l mppwidth=$width \
+    JOBID=$(qsub -l mppwidth=$width \
             -l walltime=$WALLTIME \
             -q $QUEUE \
-            -v CHROMO=$chromo,njobs=$njobs \
-            -N ${QUEUE}_${name}_${chromo}_1 \
+            -v CHROMO=$chromo,njobs=$njobs,SRCNAME=$SRCNAME\
+            -N ${QUEUE}_${name}_${chromo}  \
             -o $OUTDIR \
             -j oe \
-            submit_biases.sh
+            ${SRCNAME}.sh)
+    SCRIPT_PATH=${OUTDIR}/${SRCNAME}_${chromo}_${JOBID}.sh
+    echo "$JOBID"
+    echo "Saving to $SCRIPT_PATH"
+    cp ${SRCNAME}.sh $SCRIPT_PATH
     exit
 fi
 
@@ -59,6 +70,8 @@ module load PrgEnv-intel
 
 SIMDIR_PROPOSED=$OUTDIR/simout_${CHROMO}_${PBS_JOBID}
 SIMDIR=${SIMDIR-$SIMDIR_PROPOSED}
+PBS_FIRST_JOBID=echo $SIMDIR | sed 's/.*_//' | sed 's/\..*//'
+SCRIPT_PATH=$OUTDIR/${SRCNAME}_${CHROMO}_${PBS_FIRST_JOBID}.sh
 JOBDIR=$(echo $SIMDIR | sed 's/simout/jobout/')
 echo "SIMDIR: $SIMDIR"
 echo "JOBDIR: $JOBDIR"
@@ -84,12 +97,12 @@ if [ $PBS_MAGIC_REPEAT_NR -lt $MAX_REPEAT ]; then
     qsub -l mppwidth=$width \
         -l walltime=$PBS_WALLTIME \
         -q $PBS_O_QUEUE \
-        -v PBS_MAGIC_REPEAT_NR=$INC,CHROMO=$CHROMO,njobs=$njobs,SIMDIR=$SIMDIR \
+        -v PBS_MAGIC_REPEAT_NR=$INC,CHROMO=$CHROMO,njobs=$njobs,SIMDIR=$SIMDIR,SRCNAME=$SRCNAME \
         -W depend=afternotok:$PBS_JOBID \
         -N ${PBS_O_QUEUE}_${name}_${CHROMO}_$INC \
-        -o $OUTDIR \
+        -o $OUTDIR/${PBS_O_QUEUE}_${name}_${CHROMO}.o${PBS_FIRST_JOBID} \
         -j oe \
-        $SRCDIR/submit_biases.sh
+        $SCRIPT_PATH
 fi
 
 ################################################################################
@@ -109,7 +122,7 @@ while : ; do
 done
 
 
-run_scr=$OUTDIR/submit_biases_${CHROMO}_${PBS_JOBID}.sh
+run_scr=$JOBDIR/submit_biases_${CHROMO}_${PBS_MAGIC_REPEAT_NR}.sh
 echo "Building $run_scr..."
 cat <<SUBMIT_HELPER > ${run_scr}
 #!/bin/bash
